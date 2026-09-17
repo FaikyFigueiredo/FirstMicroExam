0ac141199391d7a249d8b91cf8f6fc86cd8201c6
Guilherme Henriques <gui.henriques500@gmail.com>
2026-09-17 17:42:26 -0300

    Create questao12.c

---
+++ questao12.c
@@ -0,0 +1,12 @@
+ #include "LCD_Blio.h"
+ 
+ void questao12() {
+      LCD_Init(4, 1);
+      char texto[50];
+      for (int i = 10; i >= 0; i--){
+          sprintf(texto,"Guilherme\nHenriques\nCavalcante\n%d", i);
+          LCD_Write_String(1, 1, texto);
+          LCD_Clear();
+      }
+
+  }
